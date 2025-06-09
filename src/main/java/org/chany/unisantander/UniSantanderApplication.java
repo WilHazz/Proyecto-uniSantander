095104f37dc3df77003844045c4c1af47e5d7e07
Hanny Paz <hwpazz@gmail.com>
2025-06-09 13:04:22 -0500

    Proyecto Back-end relizado con Springboot Java uniSantander donde tendrá alumnos, profesores, materias

---
+++ src/main/java/org/chany/unisantander/UniSantanderApplication.java
@@ -0,0 +1,13 @@
+package org.chany.unisantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSantanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSantanderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chany/unisantander/UniSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chany.unisantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSantanderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
