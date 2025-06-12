ab3e4e56ffd625483b7ce7fe027d0a7cf71ef481
Hanny Paz <hwpazz@gmail.com>
2025-06-12 18:06:00 -0500

    agregando la entidad Materia

--- src/main/java/org/chany/unisantander/Domain/Entities/Materia.java
+++ src/main/java/org/chany/unisantander/Domain/Entities/Materia.java
@@ -1,5 +1,21 @@
 package org.chany.unisantander.Domain.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "materias")
 public class Materia {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_materia")
+    private Long Id;
+    @Column(name = "nombre_materia", length = 60, nullable = false)
+    private String nombre;
 }
