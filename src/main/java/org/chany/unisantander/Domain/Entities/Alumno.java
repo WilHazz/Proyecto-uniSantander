5ab8f310d5f7209c4ad7767815545b697d029a73
Hanny Paz <hwpazz@gmail.com>
2025-06-12 17:58:48 -0500

    Realizando las entintades Alumno y Profesor con sus respectivos metodos

--- src/main/java/org/chany/unisantander/Domain/Entities/Alumno.java
+++ src/main/java/org/chany/unisantander/Domain/Entities/Alumno.java
@@ -17,14 +17,26 @@ public class Alumno{
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     @Column(name = "id_alumno")
     private Long Id;
+
+    @Column(nullable = false, unique = true, length = 10)
     private String identificacion;
+
     @Column(name = "codigo_estudiante", unique = true, length = 6)
     private String codigoEstudiante;
+
+    @Column(length = 60)
     private String nombres;
+
+    @Column(length = 60)
     private String apellidos;
+
+    @Column(length = 1)
     private String sexo;
+
     private Integer edad;
+    @Column(length = 20)
     private String contacto;
+    @Column(unique = true, length = 100)
     private String correo;
 
 
--- src/main/java/org/chany/unisantander/Domain/Entities/Profesor.java
+++ src/main/java/org/chany/unisantander/Domain/Entities/Profesor.java
@@ -1,4 +1,33 @@
 package org.chany.unisantander.Domain.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "profesores")
 public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_profesor")
+    private Long Id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String identificacion;
+    @Column(length = 50)
+    private String nombres;
+    @Column(length = 50)
+    private String apellidos;
+    @Column(length = 1, nullable = false)
+    private String sexo;
+    private Integer edad;
+    @Column(length = 10)
+    private String contacto;
+    @Column(unique = true, length = 100)
+    private String correo;
 }
