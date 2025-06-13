77a8a2cf4d4f66cf6878f0589e465719013a05c0
Hanny Paz <hwpazz@gmail.com>
2025-06-13 17:35:58 -0500

    agregando entidad materiaEstudiante

---
+++ src/main/java/org/chany/unisantander/Domain/Entities/MateriaEstudiante.java
@@ -0,0 +1,35 @@
+package org.chany.unisantander.Domain.Entities;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "materia_estudiante", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id_alumno", "id_materia", "id_periodo"})
+})
+public class MateriaEstudiante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_materia_estudiante")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_alumno", nullable = false)
+    private Alumno alumno;
+
+    @ManyToOne
+    @JoinColumn(name = "id_materia", nullable = false)
+    private Materia materia;
+
+    @ManyToOne
+    @JoinColumn(name = "id_periodo", nullable = false)
+    private Periodo periodo;
+}
--- src/main/java/org/chany/unisantander/Domain/Entities/Periodo.java
+++ src/main/java/org/chany/unisantander/Domain/Entities/Periodo.java
@@ -3,11 +3,19 @@ package org.chany.unisantander.Domain.Entities;
 import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "periodo")
+@Table(name = "periodo", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"nombre"})
+})
 public class Periodo {
 
     @Id
@@ -15,6 +23,13 @@ public class Periodo {
     @Column(name = "id_periodo")
     private Long Id;
 
+    @Column(length = 50, nullable = false)
     private String nombre;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDate fechaInicio;
+    @Column(name = "fecha_fin", nullable = false)
+    private LocalDate fechaFin;
+
 
 }
