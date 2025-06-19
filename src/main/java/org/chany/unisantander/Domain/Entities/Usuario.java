32d320904b5e6fa31937bd3c2c38be98c42d7224
Hanny Paz <hwpazz@gmail.com>
2025-06-19 17:43:49 -0500

    agregando entidades roles y usuarios

--- src/main/java/org/chany/unisantander/Domain/Entities/Usuario.java
+++ src/main/java/org/chany/unisantander/Domain/Entities/Usuario.java
@@ -1,7 +1,42 @@
 package org.chany.unisantander.Domain.Entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(table = "id_usuario")
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(unique = true, length = 100)
+    private String correo;
+
+    @Column(name = "fecha_creacion")
+    private LocalDate fechareacion = LocalDate.now();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "usuario_roles",
+            joinColumns = @JoinColumn(name = "id_usuario"),
+            inverseJoinColumns =  @JoinColumn(name = "id_rol")
+    )
+    private Set<Rol>roles;
 }
