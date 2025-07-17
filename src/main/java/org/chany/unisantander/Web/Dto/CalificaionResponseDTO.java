3ad037400b7410fffd47897e9d6f3cfb3f5fb674
Hanny Paz <hwpazz@gmail.com>
2025-07-17 18:28:24 -0500

    arreglando Dto CalificacionesREsponse

--- src/main/java/org/chany/unisantander/Web/Dto/CalificaionResponseDTO.java
+++ src/main/java/org/chany/unisantander/Web/Dto/CalificaionResponseDTO.java
@@ -4,5 +4,19 @@ import lombok.Data;
 
 @Data
 public class CalificaionResponseDTO {
-    
+    private Long id; // id_calificacion
+
+    private Long idMateriaEstudiante;
+
+    private Long idProfesorMateria;
+
+    private Double corte1;
+
+    private Double corte2;
+
+    private Double corte3;
+
+    private Double notaFinal;
+
+    private String estado; // Aprobado / Reprobado
 }
